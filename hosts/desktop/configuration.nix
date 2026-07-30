58e190d6b3c0b969fbe5530ad592f22897c9796d
is0ly <iljaosipov@gmail.com>
2026-07-30 13:57:17 +0200

    use latest Linux kernel

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -14,6 +14,7 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   nix.settings.experimental-features = [
     "nix-command"
